6e4b58d7797fa0218cde0be1079ffa0b752970bd
ivaquero <msaintms@outlook.com>
2024-09-26 18:41:37 +0800

    ox-git: clean funcs

--- ox-cli-git.ps1
+++ ox-cli-git.ps1
@@ -15,24 +15,12 @@ function get_default_branch() {
     git remote show origin | grep 'HEAD branch' | cut -d ' ' -f5
 }
 
-function git_squash {
-    git reset --soft HEAD~$args[0]
-    git add -A
-}
-
 # git republish
 function git_repub {
     git remote add origin $args[0]
     $branch_d = $(get_default_branch)
     git pull $args[0] $branch_d
     git push --set-upstream origin $branch_d
-}
-
-# git sync
-function git_sync {
-    $branch_d=$(get_default_branch)
-    git pull upstream $branch_d
-    git push origin $branch_d
 }
 
 # clean files
