9d5145b35999392067237cefdfe8a3055ce23dbf
ivaquero <msaintms@outlook.com>
2024-12-01 21:57:44 +0800

    fix: 🐛 bug in pxy()

--- ox-utils-networks.ps1
+++ ox-utils-networks.ps1
@@ -11,14 +11,15 @@ function pxy {
         $env:http_proxy = ''
         $env:all_proxy = ''
     }
-    elseif ( $(echo $the_port | wc -L) -lt 2 ) {
-        $port = $Global:OX_PROXY.$the_port
-    }
-    else {
-        $port = $the_port
-    }
-    echo "using port $($Global:OX_PROXY.$the_port)"
-    $env:https_proxy = "http://127.0.0.1:$port"
-    $env:http_proxy = "http://127.0.0.1:$port"
-    $env:all_proxy = "socks5://127.0.0.1:$port"
+    else
+        if ( $(echo $the_port | wc -L) -lt 2 ) {
+            $port = $Global:OX_PROXY.$the_port
+        }
+        else {
+            $port = $the_port
+        }
+        echo "using port $($Global:OX_PROXY.$the_port)"
+        $env:https_proxy = "http://127.0.0.1:$port"
+        $env:http_proxy = "http://127.0.0.1:$port"
+        $env:all_proxy = "socks5://127.0.0.1:$port"
 }
