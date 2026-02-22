ec50f90038c411a13ac380329a6aa02694406276
ivaquero <msaintms@outlook.com>
2026-02-23 01:17:07 +0800

    pkg_npm: simplify

--- pkg_npm.ps1
+++ pkg_npm.ps1
@@ -9,44 +9,37 @@ if ([string]::IsNullOrEmpty("$Global:OX_BACKUP/javascript")) {
     mkdir "$Global:OX_BACKUP/javascript"
 }
 
-if (Get-Command pnpm -ErrorAction SilentlyContinue ) {
-    $Global:OX_NPM = 'pnpm'
-}
-else {
-    $Global:OX_NPM = 'npm'
-}
-
 function up_node {
     Write-Output "Update Node by $($Global:OX_OXIDE.bknjx)"
     $pkgs = (cat $($Global:OX_OXIDE.bknjx) | tr '\n' ' ')
     Write-Output "Installing $pkgs"
-    Invoke-Expression ". $Global:OX_NPM install -g $pkgs --force"
+    Invoke-Expression "npm install -g $pkgs --force"
 }
 
 function back_node {
     Write-Output "Backup Node to $($Global:OX_OXIDE.bknjx)"
-    . $Global:OX_NPM list -g | rg -o '\w+@' | tr -d '@' > "$($Global:OX_OXIDE.bknjx)"
+    npm list -g | rg -o '\w+@' | tr -d '@' > "$($Global:OX_OXIDE.bknjx)"
 }
 
 ##########################################################
 # packages
 ##########################################################
 
-function ncf { . $Global:OX_NPM config $args }
-function nis { . $Global:OX_NPM install $args }
+function ncf { npm config $args }
+function nis { npm install $args }
 function nus {
     param ( $cmd )
-    Switch ( $cmd ) {
+    switch ( $cmd ) {
         pnpm { pnpm remove $args }
         npm { npm uninstall $args }
     }
 }
-function nup { . $Global:OX_NPM update $args }
-function nst { . $Global:OX_NPM outdated }
-function nsc { . $Global:OX_NPM search $args }
+function nup { npm update $args }
+function nst { npm outdated }
+function nsc { npm search $args }
 function ncl {
     param ( $cmd )
-    Switch ( $cmd ) {
+    switch ( $cmd ) {
         pnpm { pnpm cache delete $args }
         npm { npm cache clean -f $args }
     }
@@ -56,21 +49,21 @@ function ncl {
 # info
 ##########################################################
 
-function nh { . $Global:OX_NPM help $args }
+function nh { npm help $args }
 function nif { npm info }
-function nls { . $Global:OX_NPM list $args }
-function nlv { . $Global:OX_NPM list --depth 0 }
-function nck { . $Global:OX_NPM doctor }
+function nls { npm list $args }
+function nlv { npm list --depth 0 }
+function nck { npm doctor }
 
 ##########################################################
 # project
 ##########################################################
 
-function nii { . $Global:OX_NPM init $args }
-function nr { . $Global:OX_NPM run $args }
-function nts { . $Global:OX_NPM test $args }
-function npb { . $Global:OX_NPM publish $args }
+function nii { npm init $args }
+function nr { npm run $args }
+function nts { npm test $args }
+function npb { npm publish $args }
 function nfx {
-    . $Global:OX_NPM audit fix $args
-    . $Global:OX_NPM audit $args
+    npm audit fix $args
+    npm audit $args
 }
